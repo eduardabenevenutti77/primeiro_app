e670f4f3bc41823065b5b10008a1d0d1048e109e
eduardabenevenutti77 <mariaeduardabenevenutti77@gmail.com>
2022-10-13 16:05:54 -0300

    primeiro commit

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Camera</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+     <ion-grid>
+      <ion-row>
+        <ion-col size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"> </ion-img>
+        </ion-col>
+     </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+      <ion-fab-button>
+      <ion-icon :icon="camera" @click="takePhoto()"></ion-icon>
+      </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { camera, trash, close} from 'ionicons/icons';
+import {usePhotoGallery, UserPhoto}  from '@/composable/usePhotoGallery';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    const {takePhoto,photos} = usePhotoGallery();
+    return {
+      takePhoto,
+      photos,
+      camera, trash, close
+    }
+  }
+});
+</script>
---
+++ src/views/TabsPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+      
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="libraryOutline" />
+          <ion-label>Tab 1</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="telescopeOutline" />
+          <ion-label>Tab 2</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="sparklesOutline" />
+          <ion-label>Tab 3</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="sunnyOutline"/>
+          <ion-label>Tab 4</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+          <ion-icon :icon="rocketOutline" />
+          <ion-label>Tab 5</ion-label>
+        </ion-tab-button>
+
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { libraryOutline, sparklesOutline, telescopeOutline, sunnyOutline, rocketOutline } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'TabsPage',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  setup() {
+    return {
+      libraryOutline, 
+      sparklesOutline, 
+      telescopeOutline,
+      sunnyOutline,
+      rocketOutline
+    }
+  }
+});
+</script>
